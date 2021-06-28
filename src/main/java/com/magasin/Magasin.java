f762215a4cb304c6a7fde91d4d11de060c0de239
Jean-Charles Durand <jc.buchu@gmail.com>
2021-06-28 10:54:25 +0200

    First commit

---
+++ src/main/java/com/magasin/Magasin.java
@@ -0,0 +1,62 @@
+package com.magasin;
+
+class Magasin {
+    Item[] items;
+
+    public Magasin(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals("Comté")
+                    && !items[i].name.equals("Pass VIP Concert")) {
+                if (items[i].quality > 0) {
+                    if (!items[i].name.equals("Kryptonite")) {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+
+                    if (items[i].name.equals("Pass VIP Concert")) {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!items[i].name.equals("Kryptonite")) {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
+
+            if (items[i].sellIn < 0) {
+                if (!items[i].name.equals("Comté")) {
+                    if (!items[i].name.equals("Pass VIP Concert")) {
+                        if (items[i].quality > 0) {
+                            if (!items[i].name.equals("Kryptonite")) {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        items[i].quality = items[i].quality - items[i].quality;
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
