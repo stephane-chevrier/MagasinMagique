d99497816626836ad9575977f7cdb0ae9e875f08
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-03-24 10:51:23 +0100

    24/03 10h50 refactor termine, test ok, jean-charles ok

---
+++ src/main/java/com/magasin/Magasin_refactor.java
@@ -0,0 +1,73 @@
+package com.magasin;
+
+class Magasin_refactor {
+    public Item[] items;
+    private static final String COMTE = "Comté";
+    private static final String PASS_VIP_CONCERT = "Pass VIP Concert";
+    private static final String KRYPTONITE = "Kryptonite";
+
+    public Magasin_refactor(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item i : items) {
+
+            // Valeur de decrementation standard
+            int qualityDec = 1;
+            int sellInDec = 1;
+
+            // Regle date de péremption est passée, la qualité se dégrade deux fois plus rapidement.
+            if (i.sellIn <= 0) {
+                qualityDec *= 2;
+            }
+
+            // Regle Le produit "Comté" augmente sa qualité (`quality`) plus le temps passe.
+            if (i.name.equals(COMTE)) {
+                qualityDec = -qualityDec;
+            }
+
+            // Regle "Kryptonite", étant un objet légendaire, n'a pas de date de péremption
+            // et ne perd jamais en qualité (`quality`)
+            if (i.name.equals(KRYPTONITE)) {
+                qualityDec = 0;
+                sellInDec = 0;
+            }
+
+            // Regle Le "Pass VIP Concert" augmente sa qualité (`quality`) plus le temps passe (`sellIn`)
+            if (i.name.equals(PASS_VIP_CONCERT)) {
+                qualityDec = -qualityDec;
+
+                // Regle La qualité augmente de 2 quand il reste 10 jours ou moins
+                if (i.sellIn <= 10) {
+                    qualityDec = -2;
+                }
+
+                // Regle La qualité augmente de 3 quand il reste 5 jours ou moins
+                if (i.sellIn <= 5) {
+                    qualityDec = -3;
+                }
+            }
+
+            // implementation des premiers regles
+            i.sellIn -= sellInDec;
+            i.quality -= qualityDec;
+
+
+            // Regle mais la qualité tombe à 0 une fois le concert passé.
+            if ((i.sellIn < 0) && (i.name.equals(PASS_VIP_CONCERT))) {
+                i.quality = 0;
+            }
+
+            // Regle La qualité (`quality`) d'un produit n'est jamais plus grande que 50.
+            if (i.quality > 50) {
+                i.quality = 50;
+            }
+
+            // Regle La qualité (`quality`) d'un produit ne peut jamais être négative.
+            if (i.quality < 0) {
+                i.quality = 0;
+            }
+        }
+    }
+}
--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -5,55 +5,215 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MagasinTest {
 
-//    @Test
-//    void foo() {
-//        Item[] items = new Item[] { new Item("foo", 0, 0) };
-//        Magasin app = new Magasin(items);
-//        app.updateQuality();
-////        assertEquals("fixme", app.items[0].name);
-//    }
+    /**
+     * Methode d'affichage : iteration + initial/refactor + magasin
+     * @param iteration
+     * @param entete
+     * @param items
+     */
+    void afficherConsole(int iteration, String entete, Item[] items) {
+        System.out.print("iteration " + iteration + ", " + entete + " : ");
+        for (int i=0; i< items.length; i++) {
+            System.out.print(items[i].toString() + " - ");
+        }
+        System.out.println();
+    }
 
+    /**
+     * Methode de traitement : affichage magasin initial et refactor avant et apres updateQuality
+     * @param app
+     * @param app_modif
+     * @param j
+     */
+    private void traitment(Magasin app, Magasin_refactor app_modif, int j) {
+        afficherConsole(j,"initial ", app.items);
+        afficherConsole(j,"refactor  ", app_modif.items);
+        app.updateQuality();
+        app_modif.updateQuality();
+        afficherConsole(j,"initial", app.items);
+        afficherConsole(j,"refactor ", app_modif.items);
+    }
 
+    /**
+     * Methode de test d'un magasin de comte
+     */
     @Test
-    void test() {
+    void testComte() {
+
+        // initialisation variables locales
+        String product = "Comté";
+        int nombreIteration = 200;
+
+        // initialisation du magasin avec tous les cas
         Item[] items = new Item[] {
-                new Item("Comté", 100, 50),
-                new Item("Kryptonite", 100, 50),
-                new Item("Pass VIP Concert", 100, 50),
-                new Item("Jambon", 100, 50),
-                new Item("Comté", 50, 50),
-                new Item("Kryptonite", 50, 50),
-                new Item("Pass VIP Concert", 50, 50),
-                new Item("Jambon", 50, 50),
-                new Item("Comté", 100, 25),
-                new Item("Kryptonite", 100, 25),
-                new Item("Pass VIP Concert", 100, 25),
-                new Item("Jambon", 100, 25)         };
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
         Item[] items2 = new Item[] {
-                new Item("Comté", 100, 50),
-                new Item("Kryptonite", 100, 50),
-                new Item("Pass VIP Concert", 100, 50),
-                new Item("Jambon", 100, 50),
-                new Item("Comté", 50, 50),
-                new Item("Kryptonite", 50, 50),
-                new Item("Pass VIP Concert", 50, 50),
-                new Item("Jambon", 50, 50),
-                new Item("Comté", 100, 25),
-                new Item("Kryptonite", 100, 25),
-                new Item("Pass VIP Concert", 100, 25),
-                new Item("Jambon", 100, 25)        };
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
 
+        // initialisation des magasins initial / refactor
         Magasin app = new Magasin(items);
-        Magasin_modif app_modif = new Magasin_modif(items2);
+        Magasin_refactor app_modif = new Magasin_refactor(items2);
 
-        for (int j=0; j<200; j++) {
-            app.updateQuality();
-            app_modif.updateQuality();
-            System.out.print("");
+        // boucle du nombre de traitement
+        for (int j=0; j<nombreIteration; j++) {
+            traitment(app, app_modif, j);
+
+            // boucle de test sur tous les articles du magasin
             for (int i=0; i< app.items.length; i++) {
                 assertEquals(items[i].quality, items2[i].quality);
             }
         }
-        System.out.println("terminé");
+    }
+
+    /**
+     * Methode de test d'un magasin de Kryptonique
+     */
+    @Test
+    void testKryptonique() {
+
+        // initialisation variables locales
+        String product = "Kryptonique";
+        int nombreIteration = 200;
+
+        // initialisation du magasin avec tous les cas
+        Item[] items = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+        Item[] items2 = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+
+        // initialisation des magasins initial / refactor
+        Magasin app = new Magasin(items);
+        Magasin_refactor app_modif = new Magasin_refactor(items2);
+
+        // boucle du nombre de traitement
+        for (int j=0; j<nombreIteration; j++) {
+            traitment(app, app_modif, j);
+
+            // boucle de test sur tous les articles du magasin
+            for (int i=0; i< app.items.length; i++) {
+                assertEquals(items[i].quality, items2[i].quality);
+            }
+        }
+    }
+
+    @Test
+    void testPassVipConcert() {
+
+        // initialisation variables locales
+        String product = "Pass VIP Concert";
+        int nombreIteration = 200;
+
+        // initialisation du magasin de comte avec tous les cas
+        Item[] items = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+        Item[] items2 = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+
+        // initialisation des magasins initial / refactor
+        Magasin app = new Magasin(items);
+        Magasin_refactor app_modif = new Magasin_refactor(items2);
+
+        // boucle du nombre de traitement
+        for (int j=0; j<nombreIteration; j++) {
+            traitment(app, app_modif, j);
+
+            // boucle de test sur tous les articles du magasin
+            for (int i=0; i< app.items.length; i++) {
+                assertEquals(items[i].quality, items2[i].quality);
+            }
+        }
+    }
+
+    @Test
+    void testPassJambon() {
+
+        // initialisation variables locales
+        String product = "Jambon";
+        int nombreIteration = 200;
+
+        // initialisation du magasin de comte avec tous les cas
+        Item[] items = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+
+        // initialisation des magasins initial / refactor
+        Item[] items2 = new Item[] {
+                new Item(product, 100, 50),
+                new Item(product, 100, 25),
+                new Item(product, 100, 5),
+                new Item(product, 50, 50),
+                new Item(product, 50, 25),
+                new Item(product, 50, 5),
+                new Item(product, 5, 50),
+                new Item(product, 5, 25),
+                new Item(product, 5, 5)      };
+
+        Magasin app = new Magasin(items);
+        Magasin_refactor app_modif = new Magasin_refactor(items2);
+
+        // boucle du nombre de traitement
+        for (int j=0; j<nombreIteration; j++) {
+            traitment(app, app_modif, j);
+
+            // boucle de test sur tous les articles du magasin
+            for (int i=0; i< app.items.length; i++) {
+                assertEquals(items[i].quality, items2[i].quality);
+            }
+        }
     }
 }
